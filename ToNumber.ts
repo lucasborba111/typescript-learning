131e6997310f1a14f3b1cee25bc89d751c871f26
Lucas Borges Borba <69261686+lucasborba111@users.noreply.github.com>
2023-07-24 14:19:04 -0300

    Create ToNumber.ts

---
+++ ToNumber.ts
@@ -0,0 +1,9 @@
+// Transformar em número com validação
+function toNumber (numero: number|string) {
+	const tipo = typeof numero;
+	if (tipo === 'number') return numero; 
+	if (tipo === 'string') return Number(numero);
+
+	throw new Error('Tipagem incorreta');
+}
+console.log(toNumber(true));
