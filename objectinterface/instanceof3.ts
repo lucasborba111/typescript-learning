f469ed4c0e24a6fa6538588305c46549dbd26c89
lucasborba111 <lucasdbb.12@gmail.com>
2023-08-02 13:07:10 -0300

    instanceof

---
+++ objectinterface/instanceof3.ts
@@ -0,0 +1,9 @@
+// html <a id='origamid' href='http://www.origamid.com'> </a>
+const link = document.getElementById("origamid"); 
+/* get by id ira retornar a classe genérica HtmlElement mas o <a> 
+é instancia de HTMLAnchorElement e precisa fazer a verificação */
+
+if (link instanceof HTMLAnchorElement) {//verificando se é instancia, caso seja instancia ele não apontará erro
+    link.href = "https://www.origamid.com";
+}
+
