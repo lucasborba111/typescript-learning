c799680bbcb1b1a20d909f267c2469263ccc7ffe
lucasborba111 <lucasdbb.12@gmail.com>
2023-08-03 13:29:58 -0300

    changes

--- objectinterface/interfacedom/interfacedom1.ts
+++ objectinterface/interfacedom/interfacedom1.ts
@@ -3,7 +3,6 @@ const nodeList = document.querySelectorAll<HTMLElement>(".link");
 function changeColorAndBorder(linkArray: HTMLElement[]) {
   linkArray.forEach((element) => {
     if (element instanceof HTMLElement) element.style.border = "#FF0000 solid 1px";
-    console.log("aaaa");
   });
 }
 
