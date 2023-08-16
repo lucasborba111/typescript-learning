7e30e7b79808934dcb101bf5941a9221c4b30175
lucasborba111 <lucasborgesborba@gmail.com>
2023-08-16 20:04:38 -0300

    indentação

--- objectinterface/exercicio.ts
+++ objectinterface/exercicio.ts
@@ -1,53 +1,53 @@
 interface Window {
-    UserData: any
+  UserData: any
 };
 
 window.UserData = {};
 
 interface UserData {
-    nome?: String;
-    email?: String;
-    cpf?: String;
+  nome?: String;
+  email?: String;
+  cpf?: String;
 }
 
 function isUserData(obj: unknown): obj is UserData {
-    if (obj && typeof obj === 'object' && ('nome' in obj || 'email' in obj || 'cpf' in obj)) {
-        return true
-    } else {
-        return false
-    }
+  if (obj && typeof obj === 'object' && ('nome' in obj || 'email' in obj || 'cpf' in obj)) {
+    return true
+  } else {
+    return false
+  }
 }
 
 const cbFunction = function ({ target }: KeyboardEvent) {
-    if (target instanceof HTMLInputElement) {
-        window.UserData[target.id] = target.value;
-        localStorage.setItem('UserData', JSON.stringify(window.UserData)); 
-    }
+  if (target instanceof HTMLInputElement) {
+    window.UserData[target.id] = target.value;
+    localStorage.setItem('UserData', JSON.stringify(window.UserData)); 
+  }
 }
 
 function validJSON(str: string) {
-    try {
-      JSON.parse(str);
-    } catch (e) {
-      return false;
-    }
-    return true;
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
 }
 
 function loadLocalStorage() {
-    const localUserData = localStorage.getItem('UserData');
-    if (localUserData && validJSON(localUserData)) {
-      const UserData = JSON.parse(localUserData);
-      if (isUserData(UserData)) {
-        Object.entries(UserData).forEach(([key, value]) => {
-          const input = document.getElementById(key);
-          if (input instanceof HTMLInputElement) {
-            input.value = value;
-            window.UserData[key] = value;
-          }
-        });
-      }
+  const localUserData = localStorage.getItem('UserData');
+  if (localUserData && validJSON(localUserData)) {
+    const UserData = JSON.parse(localUserData);
+    if (isUserData(UserData)) {
+      Object.entries(UserData).forEach(([key, value]) => {
+        const input = document.getElementById(key);
+        if (input instanceof HTMLInputElement) {
+          input.value = value;
+          window.UserData[key] = value;
+        }
+      });
     }
+  }
 }
 
 loadLocalStorage();
